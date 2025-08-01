de945fd7c648ac31088a0ad04df8b2d47c3e5d83
rukevweubiodataengineering <ubiowororuki@gmail.com>
2025-08-01 14:34:14 +0000

    update the terraform script

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,12 +34,24 @@ resource "aws_subnet" "public_subnet" {
     tags = { Name = "public-subnet" }
 }
 
-resource "aws_subnet" "private_subnet" {
-    vpc_id            = aws_vpc.main_vpc.id
-    cidr_block        = "10.0.2.0/24"
-    availability_zone = "us-east-1a"
-    tags = { Name = "private-subnet" }
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "private-subnet-1"
+  }
 }
+
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "us-east-1b"
+  tags = {
+    Name = "private-subnet-2"
+  }
+}
+
 
 resource "aws_route_table" "public_rt" {
     vpc_id = aws_vpc.main_vpc.id
@@ -75,10 +87,16 @@ resource "aws_route_table" "private_rt" {
     tags = { Name = "private-route-table" }
 }
 
-resource "aws_route_table_association" "private_assoc" {
-    subnet_id      = aws_subnet.private_subnet.id
-    route_table_id = aws_route_table.private_rt.id
+resource "aws_route_table_association" "private_assoc_1" {
+  subnet_id      = aws_subnet.private_subnet_1.id
+  route_table_id = aws_route_table.private_rt.id
 }
+
+resource "aws_route_table_association" "private_assoc_2" {
+  subnet_id      = aws_subnet.private_subnet_2.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
 
 resource "aws_security_group" "frontend_sg" {
     name   = "frontend-sg"
@@ -148,10 +166,14 @@ resource "aws_instance" "frontend" {
 }
 
 resource "aws_db_subnet_group" "db_subnet_group" {
-    name       = "db-subnet-group"
-    subnet_ids = [aws_subnet.private_subnet.id]
-    tags = { Name = "MySQL Subnet Group" }
+  name       = "db-subnet-group"
+  subnet_ids = [
+    aws_subnet.private_subnet_1.id,
+    aws_subnet.private_subnet_2.id
+  ]
+  tags = { Name = "MySQL Subnet Group" }
 }
+
 
 resource "aws_db_instance" "mysql" {
     allocated_storage       = 20
