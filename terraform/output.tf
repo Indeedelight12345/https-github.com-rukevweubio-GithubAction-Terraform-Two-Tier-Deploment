a8ee02c5224162408bb947df49dd29cc00ef3376
rukevweubiodataengineering <ubiowororuki@gmail.com>
2025-08-02 06:49:05 +0000

    update the ec2 public ip address

--- terraform/output.tf
+++ terraform/output.tf
@@ -8,3 +8,14 @@ output "rds_instance_id" {
     description = "The ID of the RDS database instance."
     value       = aws_db_instance.mysql.id
 }
+
+output "rds_endpoint" {
+  description = "The endpoint of the RDS database instance."
+  value       = aws_db_instance.mysql.endpoint
+}
+
+
+output "public_ip" {
+  description = "Public IP of the frontend EC2 instance"
+  value       = aws_instance.frontend.public_ip
+}
