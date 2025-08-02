f4d405d4dcf5716986974d567ebbcad5b9c10dc0
rukevweubiodataengineering <ubiowororuki@gmail.com>
2025-08-02 08:55:22 +0000

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,7 +20,7 @@ resource "aws_key_pair" "my_key" {
 
 
 resource "aws_vpc" "main_vpc" {
-    cidr_block = "10.0.0/16"
+    cidr_block = "10.0.0.0/16"
     enable_dns_support = true
     enable_dns_hostnames = true
     tags = { Name = "main-vpc" }        
