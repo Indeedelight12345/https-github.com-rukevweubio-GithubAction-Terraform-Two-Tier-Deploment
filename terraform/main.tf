82521e6e2b0c059e50c18d6126ef6abf376de157
rukevweubiodataengineering <ubiowororuki@gmail.com>
2025-08-02 07:27:23 +0000

    update the code

--- terraform/main.tf
+++ terraform/main.tf
@@ -189,7 +189,7 @@ resource "aws_db_instance" "mysql" {
     #name                    = "mydb"
     username                = "admin"
     password                = var.db_password
-    db_subnet_group_name    = .aws_db_subnet_group.db_subnet_group-name.name
+    db_subnet_group_name    = aws_db_subnet_group.db_subnet_group-name.name
     vpc_security_group_ids  = [aws_security_group.rds_sg.id]
     publicly_accessible     = false
     skip_final_snapshot     = false
